a271b53c5459d79ce03e5a0431875199349daff1
Bez04 <bezalelmathew659@gmail.com>
2025-09-26 05:48:02 +0100

    Add files via upload

---
+++ biggest_3_number.py
@@ -0,0 +1,12 @@
+# FIND THE BIGGEST NUMBER
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=int(input("Enter the third number: "))
+if(a>b)and(a>c):
+    largest=a
+elif(b>a)and(b>c):
+    largest=b
+else:
+    largest=c
+    print("The Biggest Number Is: ",largest)        
