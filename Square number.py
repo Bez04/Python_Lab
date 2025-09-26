0435647df5fcfd911b981be709b3ed39b40cbe2b
Bez04 <bezalelmathew659@gmail.com>
2025-09-26 05:56:33 +0100

    Add files via upload

---
+++ Square number.py
@@ -0,0 +1,10 @@
+import math
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# calculate square root
+sqrt = math.sqrt(num)
+
+# display result
+print("The square root of", num, "is:", sqrt)
---
+++ integer_n.py
@@ -0,0 +1,7 @@
+num=input("Enter The Integer: ")
+int1=int(num)
+int2=int(num*2)
+int3=int(num*3)
+
+result=int1+int2+int3
+print(f"Result is {int1} + {int2} + {int3} = {result}")
