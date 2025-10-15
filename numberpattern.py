648b8daa879e037bc39eeeaccadb8ef6bd99c960
Bez04 <bezalelmathew659@gmail.com>
2025-10-15 11:30:33 +0530

    Add files via upload

---
+++ numberpattern.py
@@ -0,0 +1,6 @@
+n=int(input("enter the limit:"))
+for i in range(1,n):
+    print(str(i)*i)
+
+    
+   
---
+++ pattren.py
@@ -0,0 +1,16 @@
+# normal pattern
+
+for i in range(6):
+    print("*"*6)
+
+# Star Pattern Printing 
+
+n=int(input("enter the limit:"))
+for i in range(1,n+1):
+    print(i*"*")
+
+# Star pattern reverse order
+
+n=int(input("enter the limit:"))
+for i in range(n,0,-1):
+    print(i*"*")
