abde71122f6ad40da450fd620b7c58e20848d26c
Bez04 <bezalelmathew659@gmail.com>
2025-11-03 11:13:52 +0530

    Add files via upload

---
+++ rectangle Area(class&peri).py
@@ -0,0 +1,17 @@
+def getdata(self,length,breadth):
+    self.length=length
+    self.breadth=breadth
+def area(self):
+    a=self.length*self.breadth
+print("Area of rectangle with length",self.length,"and breadth ",self.breadth ,"is ",a)
+def perimeter(self):
+    p=2(self.length+self.breadth)
+print("perimeter of rectangle with length",self.length,"and breadth ",self.breadth ,"is ",p)
+
+ch=0
+l=int(input("Enter a length:"))
+b=int(input("Enter a breadth:"))
+obj=rectangle()
+obj.getdata(l,b)
+while ch1=3
+ print()
