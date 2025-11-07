1bfd65d70c0f3648b3eb6aa4ae7d2eb87d26198a
Bezalel Mathew <bezalelmathew659@gmail.com>
2025-11-07 10:34:35 +0530

    Refactor rectangle class and add user menu
    Refactor rectangle class to improve area and perimeter methods, and add a menu for user interaction.

--- rectangle Area(class&peri).py
+++ rectangle Area(class&peri).py
@@ -1,17 +1,36 @@
-def getdata(self,length,breadth):
-    self.length=length
-    self.breadth=breadth
-def area(self):
-    a=self.length*self.breadth
-print("Area of rectangle with length",self.length,"and breadth ",self.breadth ,"is ",a)
-def perimeter(self):
-    p=2(self.length+self.breadth)
-print("perimeter of rectangle with length",self.length,"and breadth ",self.breadth ,"is ",p)
+class rectangle:
+    def getdata(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
 
-ch=0
-l=int(input("Enter a length:"))
-b=int(input("Enter a breadth:"))
-obj=rectangle()
-obj.getdata(l,b)
-while ch1=3
- print()
+    def area(self):    
+        print("Area of rectangle:", self.length * self.breadth)
+
+    def perimeter(self):
+        print("Perimeter of rectangle:", 2 * (self.length + self.breadth))
+
+
+# Main program
+l = float(input("Enter a length: "))
+b = float(input("Enter a breadth: "))
+
+obj = rectangle()
+obj.getdata(l, b)
+
+ch = 0
+while ch != 3:
+    print("\n*------ Menu ------*")
+    print("1. Area")
+    print("2. Perimeter")
+    print("3. Exit")
+
+    ch = int(input("Enter your choice: "))
+
+    if ch == 1:
+        obj.area()
+    elif ch == 2:
+        obj.perimeter()
+    elif ch == 3:
+        print("Exiting Program...")
+    else:
+        print("Invalid choice!!")
