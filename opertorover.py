62ff27612a3e551e8973b00e405599be553d7d8d
Bezalel Mathew <bezalelmathew659@gmail.com>
2025-11-14 10:50:46 +0530

    Add files via upload

---
+++ opertorover.py
@@ -0,0 +1,8 @@
+class Number:
+    def __init__(self,value):
+        self.value=value
+    def _add_(self,other):
+       return (self.value+ self.other)
+n1=Number(10)
+n2=Number(20)
+print(n1+n2)    
---
+++ time overload.py
@@ -0,0 +1,30 @@
+class Time:
+    def __init__(self, h, m, s):
+        self.__hour = h
+        self.__minute = m
+        self.__second = s
+
+    def __add__(self, other):
+        h = self.__hour + other.__hour
+        m = self.__minute + other.__minute
+        s = self.__second + other.__second
+
+
+        if s >= 60:
+            m += s // 60
+            s = s % 60
+        if m >= 60:
+            h += m // 60
+            m = m % 60
+
+        return Time(h, m, s)
+
+    def show(self):
+        print(self.__hour, ":", self.__minute, ":", self.__second)
+
+
+t1 = Time(2, 50, 40)
+t2 = Time(1, 20, 30)
+t3 = t1 + t2
+
+t3.show()
